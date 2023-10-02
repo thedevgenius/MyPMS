f5b48e3857fa7f090a407e3fe69862283dacf412
Dev Genius Home <thedevgenius83@gmail.com>
2023-10-02 14:53:54 +0530

    Added

--- blogging/settings.py
+++ blogging/settings.py
@@ -128,9 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
